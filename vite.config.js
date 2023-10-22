5f2b714b72ea6d36a76cd75e34f57f77322f9a8d
Cipher <dipanshusaini69@gmail.com>
2023-10-22 14:20:38 +0530

    clean up after prev mess of 3d models

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    // virtual({
-    //   "**/*.glb": "", // Handle GLB files as virtual modules
-    // }),
-  ],
+  plugins: [react()],
   base: "/portfolio/",
-
-  build: {
-    rollupOptions: {
-      output: {
-        assetsInclude: ["**/*.glb"],
-      },
-    },
-  },
 });
